eb1835e8d72e966b3f0d568666a1a2bd5690437f
Hugo Torres <hugo@canarytek.com>
2025-05-07 12:23:23 +0100

    Cambiado nombre variable seller a id_seller en la ruta de buy.

--- routes/web.php
+++ routes/web.php
@@ -46,8 +46,8 @@ Route::get('servicio/ver/{id_servicio}', [ServiceController::class,'mostrar']);
 
 // Compra y venta de tokens
 Route::middleware('auth')->group(function () {
-    Route::get('/comprar/{cantidad_tokens}/{precio_tokens}/{seller?}', function ($cantidad_tokens, $precio_tokens, $seller = null) {
-        return view('buy', compact('cantidad_tokens', 'precio_tokens', 'seller'));
+    Route::get('/comprar/{cantidad_tokens}/{precio_tokens}/{id_seller?}', function ($cantidad_tokens, $precio_tokens, $id_seller = null) {
+        return view('buy', compact('cantidad_tokens', 'precio_tokens', 'id_seller'));
     });
     
 
