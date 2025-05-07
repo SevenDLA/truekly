d790ee987d05c7309e3b2998017502a9ba6d98fb
Hugo Torres <hugo@canarytek.com>
2025-05-07 14:29:11 +0100

    Oferta solo sale si su estado estado está en venta.

--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -7,15 +7,18 @@
 
 <ul>
     @foreach ($ofertas as $oferta)
-        <li>
-            <strong>ID oferta: {{ $oferta->id }} </strong>
-            <p>Cantidad de tokens: {{ $oferta->tokens }} </p>
-            <p>Precio a vender: {{ $oferta->price }} €</p>
-            <p>Nombre del seller: {{ $oferta->seller->username }}</p>
-            <p>Estado de la oferta: {{ $ESTADO[$oferta->status] }}</p>
-            
-            <a class ="btn btn-success" href="/comprar/{{ $oferta->tokens }}/{{ $oferta->price }}/{{ $oferta->seller->id }}/{{ $oferta->id }}">Comprar tokens</a>
-        </li>
+        @if($oferta->status == 'E')
+            <li>
+                <strong>ID oferta: {{ $oferta->id }} </strong>
+                <p>Cantidad de tokens: {{ $oferta->tokens }} </p>
+                <p>Precio a vender: {{ $oferta->price }} €</p>
+                <p>Nombre del seller: {{ $oferta->seller->username }}</p>
+
+                <p>Estado de la oferta: {{ $ESTADO[$oferta->status] }}</p>
+                
+                <a class ="btn btn-success" href="/comprar/{{ $oferta->tokens }}/{{ $oferta->price }}/{{ $oferta->seller->id }}/{{ $oferta->id }}">Comprar tokens</a>
+            </li>
+        @endif
     @endforeach
 </ul>
 
